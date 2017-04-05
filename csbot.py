6647bf2e99312893359fd91a8efa0519a5a2c4cf
mrZizik <mrfreezzzer@mail.ru>
2017-04-05 11:29:04 +0300

    bot init

--- csbot.py
+++ csbot.py
@@ -1,9 +1,44 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals 
+from flask import Flask, request
+from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
+import telegram
+
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
-def hello_world():
-    return "It's working"
+TOKEN = "344036403:AAE9H0YR9_bTlw5uQm6PUzKG97ziq4GUdBQ"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+global bot 
+bot = telegram.Bot(token=TOKEN)
+
+URL = '95.85.39.36' 
+
+#WebHook
+@app.route('/HOOK', methods=['POST', 'GET']) 
+def webhook_handler():
+    if request.method == "POST": 
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        try:
+            chat_id = update.message.chat.id 
+            text = update.message.text
+            userid = update.message.from_user.id
+            username = update.message.from_user.username
+            bot.send_message(chat_id=chat_id, text="fuck")
+except Exception, e:
+            print e
+    return 'ok' 
+
+#Set_webhook 
+@app.route('/set_webhook', methods=['GET', 'POST']) 
+def set_webhook(): 
+    s = bot.setWebhook('https://%s:443/HOOK' % URL, certificate=open('/etc/ssl/server.crt', 'rb')) 
+    if s:
+        print(s)
+        return "webhook setup ok" 
+    else: 
+        return "webhook setup failed" 
+
+@app.route('/') 
+def index(): 
+    return '<h1>Hello</h1>' 
